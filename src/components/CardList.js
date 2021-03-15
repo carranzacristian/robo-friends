1cb1c2b6e7ee180406389f08c74f0859f3505ddc
cristian carranza <hncristiansoriano@users.noreply.github.com>
2021-03-14 23:38:30 -0400

    adding files

---
+++ src/components/CardList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+	return(
+		<div>
+	    	{
+	    		//could've simply used user.key
+	    		//for learning purposes I'll keep robots[i].key
+		    	robots.map((user, i) => {
+					return (
+						<Card
+						key={robots[i].id}
+						id={robots[i].id}
+						name={robots[i].name}
+						email={robots[i].email}
+						/>
+					);
+				})
+	    	}
+    	</div>
+	);
+}
+
+export default CardList;
---
+++ src/containers/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import  './App.css'
+import loadingGif from './Wedges-3s-200px.gif';
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+			robots: [],
+			searchfield: '',
+		}
+	}
+
+	componentDidMount() {
+		fetch('https://jsonplaceholder.typicode.com/users')
+			.then(response =>  response.json())
+			.then(users => this.setState({ robots: users }));
+	}
+
+	onSearchChange = (event) =>{
+		this.setState({ searchfield: event.target.value })
+	}
+
+	render() {
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+		})
+		return !robots.length ?
+			(
+			<div className='tc container'>
+				<img className='gif' src={ loadingGif } alt="" />
+				<h1>Loading</h1>
+			</div> 
+			):
+			(
+			<div className='tc container'>
+				<h1 className='f1'>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<Scroll>
+					<CardList robots={filteredRobots}/>
+				</Scroll>
+			</div>
+			);
+	}
+}
+
+export default App;
