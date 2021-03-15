df6caaf5a6ce6f2e627ac4520c5a437049f1fa41
cristian carranza <hncristiansoriano@users.noreply.github.com>
2021-03-15 00:53:11 -0400

    Added Error Boundary

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import  './App.css'
 import loadingGif from './Wedges-3s-200px.gif';
 
@@ -41,7 +42,9 @@ class App extends Component {
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={this.onSearchChange}/>
 				<Scroll>
-					<CardList robots={filteredRobots}/>
+					<ErrorBoundry>
+						<CardList robots={filteredRobots}/>
+					</ErrorBoundry>
 				</Scroll>
 			</div>
 			);
